8ddb938b24f49749d1f688fb9a124209a491ba79
zuhair2025 <zuhair2025@gmail.com>
2025-07-29 00:39:05 +0600

    added Service CRUD

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ServiceRequest;
+use App\Http\Resources\ServiceResource;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function index()
+    {
+        $services = Service::where('status','active')->get();
+
+        return ServiceResource::collection($services);
+    }
+
+    public function store(ServiceRequest $request)
+    {
+        $service = Service::create($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Service Created Successfully',
+            'data'  => new ServiceResource($service)
+        ]);
+    }
+
+    public function update(ServiceRequest $request, Service $service)
+    {
+        $service->update($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Service updated successfully',
+            'service' => new ServiceResource($service)
+        ]);
+    }
+
+    public function destroy(Service $service)
+    {
+        $service->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data has been deleted successfully',
+        ],200);
+    }
 }
