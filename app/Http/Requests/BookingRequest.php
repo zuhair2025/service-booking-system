7ea9eb3b3d969d865473404a33059fd10d9905e9
zuhair2025 <zuhair2025@gmail.com>
2025-07-29 12:30:38 +0600

    Booking process done

---
+++ app/Http/Requests/BookingRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'service_id'   => 'required|exists:services,id',
+            'booking_date' => 'required|date|after_or_equal:today',
+        ];
+    }
+}
